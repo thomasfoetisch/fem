5affc41a5e75d13a528eed90099a23d1e6e0b8fc
Thomas Foetisch <thomas.foetisch@epfl.ch>
2015-10-20 21:29:29 +0200

    Forgot that file. Build the change of basis between geometrical space and normal space on the boundary.

---
+++ +stokes2d/build_dof_rotation_matrix.m
@@ -0,0 +1,36 @@
+function [rot, rot_inv] = build_dof_rotation_matrix(mesh, nodes, new_basis)
+
+  % problem sizes:
+  n_nodes = size(mesh.nodes, 1);
+  n_elems = size(mesh.elements, 1);
+  n_u_dof = n_nodes + n_elems;
+  n_p_dof = n_nodes;
+  n_dof = 2 * n_u_dof + n_p_dof;
+
+  % normal homogeneous dirichlet conditions:
+  rot = spalloc(n_dof, n_dof, 0);
+  rot_inv = spalloc(n_dof, n_dof, 0);
+
+  % initialize with the identity matrix:
+  rot(sub2ind(size(rot), 1:n_dof, 1:n_dof)) = 1;
+  rot_inv(sub2ind(size(rot_inv), 1:n_dof, 1:n_dof)) = 1;
+  
+  % loop over all the selected boundary nodes:
+  for node = 1:length(nodes);
+    local_rot = new_basis(:, :, node);
+    local_rot_inv = inv(local_rot);
+
+    node_id = mesh.boundary_nodes(nodes(node));
+
+    % assemble the rotation matrix and the related inverse rotation:
+    rot(node_id, node_id) = local_rot(1, 1);
+    rot(node_id, n_u_dof + node_id) = local_rot(1, 2);
+    rot(n_u_dof + node_id, node_id) = local_rot(2, 1);
+    rot(n_u_dof + node_id, n_u_dof + node_id) = local_rot(2, 2);
+
+    rot_inv(node_id, node_id) = local_rot_inv(1, 1);
+    rot_inv(node_id, n_u_dof + node_id) = local_rot_inv(1, 2);
+    rot_inv(n_u_dof + node_id, node_id) = local_rot_inv(2, 1);
+    rot_inv(n_u_dof + node_id, n_u_dof + node_id) = local_rot_inv(2, 2);
+  end
+  
